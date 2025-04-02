67f8be890268f21da9f60e02ae753e139a3363a9
son <sssk1755@gmail.com>
2025-04-02 16:25:51 +0900

    -view complete

--- src/main/java/shop/mtcoding/blog/board/BoardController.java
+++ src/main/java/shop/mtcoding/blog/board/BoardController.java
@@ -1,5 +1,8 @@
 package shop.mtcoding.blog.board;
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,7 +25,12 @@ public class BoardController {
     }
 
     @GetMapping("/board/{id}/updateForm")
-    public String updateForm(@PathVariable int id) {
+    public String updateForm(@PathVariable int id, HttpServletRequest request, HttpServletResponse response) {
+        Cookie cookie = new Cookie("boardId", id + "");
+        cookie.setHttpOnly(true);
+        response.addCookie(cookie);
+
+        request.setAttribute("boardId", id);
         return "board/updateForm";
     }
 }
